b715072ca7f40e7e0acfe43a3405eb67080d378e
GDJ 58 <GDJ 58@DESKTOP-C8KOHDO>
2022-11-21 10:11:26 +0900

    vo 생성(도메인타입)

---
+++ src/main/java/vo/Cash.java
@@ -0,0 +1,47 @@
+package vo;
+
+public class Cash {
+	private int cashNo;
+	// private Category category;
+	private int categoryNo; // FK -> join을 사용 -> HashMap타입 
+	private long cashPrice; // 합계를 구하게 되면 숫자가 커질 수 있기 때문에 long타입 설정
+	private String memo;
+	private String updatedate;
+	private String createdate;
+	public int getCashNo() {
+		return cashNo;
+	}
+	public void setCashNo(int cashNo) {
+		this.cashNo = cashNo;
+	}
+	public int getCategoryNo() {
+		return categoryNo;
+	}
+	public void setCategoryNo(int categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+	public long getCashPrice() {
+		return cashPrice;
+	}
+	public void setCashPrice(long cashPrice) {
+		this.cashPrice = cashPrice;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+}
